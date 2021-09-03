7a11d2d3ea1c262eb18d120b01f97a7667a9947e
Chris <Chris@Cloudyde-MacBook-Pro.local>
2021-09-03 17:00:15 +0800

    20210903 gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
 
+export interface IState {
+  people: {
+      name: string
+      age: number
+      img: string
+      note?: string
+  }[]
+}
 function App() {
+  const [people,setPeople]=useState<IState["people"]>([
+    {
+      name:"LBJ",
+      age:36,
+      img:"https://p.kindpng.com/picc/s/110-1109845_lebron-james-lakers-flex-hd-png-download.png",
+      note:"HI 23"
+    },
+    {
+      name:"KOBE",
+      age:40,
+      img:"https://www.pngitem.com/pimgs/m/1-14685_kobe-png-transparent-png.png",
+      note:"HI 24"
+    }
+  ])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>All Start Party</h1>
+     <List people={people}/>
+     <AddToList setPeople={setPeople} people={people}/>
     </div>
   );
 }
